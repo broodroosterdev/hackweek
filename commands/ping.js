9f7e6b00a93567c489f7068d7801ec85e868c8f7
ScientistMonkey <35115220+ScientistMonkey@users.noreply.github.com>
2019-06-24 20:11:29 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
+    message.delete(1000);
+
       // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
       // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
       const m = await message.channel.send("Pong?");
