4693a802c743f28266aaa64e871b6efa9cb4ad78
ScientistMonkey <35115220+ScientistMonkey@users.noreply.github.com>
2019-06-24 22:05:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const fs = require('fs');
 const active = new Map();
+const game = require('./game.js');
 
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
