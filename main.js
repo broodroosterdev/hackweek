076235876c62f0a3a27dcf9eab45019a3ba22566
ScientistMonkey <35115220+ScientistMonkey@users.noreply.github.com>
2019-06-24 19:37:23 +0200

    Command handler

--- main.js
+++ main.js
@@ -16,6 +16,32 @@ const FieldValue = require('firebase-admin').firestore.FieldValue;
 const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccount.json');
 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  })
+  
+  let db = admin.firestore();
+
+
+// COMMAND HANDLER 
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) return console.log(err);
+  
+    let cmdFiles = files.filter(f => f.split(".").pop() === "js")
+    if (cmdFiles.length <= 0) {
+      console.log("Couldn't find commands.");
+      return;
+    }
+  
+    cmdFiles.forEach((f, i) => {
+      let props = require(`./commands/${f}`);
+      console.log(`${i+1}: ${f} loaded!`);
+      bot.commands.set(props.help.name, props);
+    })
+  
+  })
+
 bot.on('ready', () => {
     console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`);
     setInterval(() => {
