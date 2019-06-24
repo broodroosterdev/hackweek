bdee7221d82cc9f1bf1479fb0073c222c88a38f9
ScientistMonkey <35115220+ScientistMonkey@users.noreply.github.com>
2019-06-24 22:37:32 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,18 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
+    let sIcon = message.guild.iconURL;
+    message.delete(250)
+    console.log(`${message.author.tag}, requested help in ${message.guild.name}`)
+    let helpEmbed = new Discord.RichEmbed()
+    .setTitle(`**Help commands for Hackweek**`)
+    .setColor(botC)
+    .setThumbnail(sIcon)
+    .addField("**Moderation**", `**Description:** All the fancy commands to bully other members\n**Usage:** *[prefix]* help moderation`)
+    .addField("**Moosik**", `**Description:** `)
+    .setTimestamp(new Date())
+    .setFooter(`Requested by ${message.author.tag}`, `${message.author.avatarURL}`)
 
+    message.author.send(helpEmbed)
     
 }
 
