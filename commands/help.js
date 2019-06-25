f4a6fb27b6db2601a2f6b0fdc5ebad6b9f85bf26
ScientistMonkey <35115220+ScientistMonkey@users.noreply.github.com>
2019-06-25 03:26:24 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,20 +1,76 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let sIcon = message.guild.iconURL;
-    message.delete(250)
-    console.log(`${message.author.tag}, requested help in ${message.guild.name}`)
-    let helpEmbed = new Discord.RichEmbed()
-    .setTitle(`**Help commands for Hackweek**`)
+let helpEmbed = new Discord.RichEmbed()
+    .setTitle("Help")
     .setThumbnail(sIcon)
-    .addField("**Moderation**", `**Description:** All the fancy commands to bully other members\n**Usage:** *[prefix]* help moderation`)
-    .addField("**Moosik**", `**Description:** `)
+    .addField(`**bruh**`)
+    .setDescription("test purposes")
     .setTimestamp(new Date())
     .setFooter(`Requested by ${message.author.tag}`, `${message.author.avatarURL}`)
+            
+    const m = await message.channel.send(helpEmbed);
 
-    message.author.send(helpEmbed)
+    await m.react('❓');
+    await m.react('🎮');
+  await m.react('🎵');
+  await m.react('🛠');
+
+  const filter = (reaction, user) => {
+    return ['🎮', '🎵', '🛠', '❓'].includes(reaction.emoji.name) && user.id === message.author.id;
+  }
+
+  const collector = m.createReactionCollector(filter, {
+    time: 60000
+  });
+  
+  collector.on('collect', reaction => {
+    reaction.remove(message.author)
+
+    switch (reaction.emoji.name) {
+            case '🎮':
+                let gamesEmbed = new Discord.RichEmbed()
+                    .setTitle(`**Help commands for Hackweek**`)
+                    .setThumbnail(sIcon)
+                    .addField(`**GAMES**`)
+                    .setTimestamp(new Date())
+                    .setFooter(`Requested by ${message.author.tag}`, `${message.author.avatarURL}`)
+        
+                    m.edit(gamesEmbed);
+                break;
+
+            case '🎵':
+                let musicEmbed = new Discord.RichEmbed()
+                .setTitle(`**Help commands for Hackweek**`)
+                    .setThumbnail(sIcon)
+                    .addField(`**MUSIC**`)
+                    .setTimestamp(new Date())
+                    .setFooter(`Requested by ${message.author.tag}`, `${message.author.avatarURL}`)
+        
+                    m.edit(musicEmbed);
+                break;
+
+            case '🛠':
+                let moderationEmbed = new Discord.RichEmbed()
+            .setTitle(`**Help commands for Hackweek**`)
+            .setThumbnail(sIcon)
+            .addField(`**MODERATION**`)
+            .setTimestamp(new Date())
+            .setFooter(`Requested by ${message.author.tag}`, `${message.author.avatarURL}`)
+
+            m.edit(moderationEmbed);
+                break;
+
+            case '❓':
+                    
+                    m.edit(helpEmbed);
+                break;
+        }
+        
+    }
+)}
     
-}
-
-module.exports.help = {
-  name: "help"
-}
+    module.exports.help = {
+      name: "help"
+    }
+    
