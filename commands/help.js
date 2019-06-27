1845d0f31e2c40b46fbeb8d8879690e94cc32fcb
broodroosterdev <31385368+broodroosterdev@users.noreply.github.com>
2019-06-27 13:19:38 +0200

    Added working score to help command

--- commands/help.js
+++ commands/help.js
@@ -2,18 +2,18 @@ const Discord = require("discord.js");
 const info = require("../game.js");
 module.exports.run = async (bot, message, args) => {
     let sIcon = message.guild.iconURL;
-        let helpEmbed = new Discord.RichEmbed()
-            .setTitle("Help")
-            .setDescription("Click on the corresponding emoji to get help about that object")
-            .setThumbnail(sIcon)
-            .addField("📋 Profile", "Get info about your scores", true)
-            .addField(`🎮 Games`, "Let's play some games!", true)
-            // .addBlankField(false, true)
-            .addField("🎵 Music", "Chill or rock, you choose", true)
-            .addField("🛠 Moderation", "Power abuse?", true)
-            .setTimestamp(new Date())
-            .setFooter(`Requested by ${message.author.tag}`, `${message.author.avatarURL}`)
-            
+    let helpEmbed = new Discord.RichEmbed()
+        .setTitle("Help")
+        .setDescription("Click on the corresponding emoji to get help about that object")
+        .setThumbnail(sIcon)
+        .addField("📋 Profile", "Get info about your scores", true)
+        .addField(`🎮 Games`, "Let's play some games!", true)
+        // .addBlankField(false, true)
+        .addField("🎵 Music", "Chill or rock, you choose", true)
+        .addField("🛠 Moderation", "Power abuse?", true)
+        .setTimestamp(new Date())
+        .setFooter(`Requested by ${message.author.tag}`, `${message.author.avatarURL}`)
+
     const m = await message.channel.send(helpEmbed);
 
     await m.react('📋');
@@ -21,56 +21,69 @@ module.exports.run = async (bot, message, args) => {
     await m.react('🎵');
     await m.react('🛠');
 
-  const filter = (reaction, user) => {
-    return ['📋', '🎮', '🎵', '🛠'].includes(reaction.emoji.name) && user.id === message.author.id;
-  }
+    const filter = (reaction, user) => {
+        return ['📋', '🎮', '🎵', '🛠'].includes(reaction.emoji.name) && user.id === message.author.id;
+    }
 
-  const collector = m.createReactionCollector(filter, {
-    time: 60000
-  });
-  
-//   let playerData = this.bot.db.collection('players').doc(this.player.id.toString());
-//   await playerData.get().then(doc => {
-    collector.on('collect', reaction => {
-      reaction.remove(message.author)
+    const collector = m.createReactionCollector(filter, {
+        time: 60000
+    });
+
+    //   let playerData = this.bot.db.collection('players').doc(this.player.id.toString());
+    //   await playerData.get().then(doc => {
+    collector.on('collect', async reaction => {
+        reaction.remove(message.author)
 
 
 
-                //   if (!doc.exists) {
+        //   if (!doc.exists) {
 
-                //       console.log('No such document!');
+        //       console.log('No such document!');
 
-                //   } else {
+        //   } else {
 
-                //       console.log('Document data:', doc.data());
+        //       console.log('Document data:', doc.data());
 
-                //       console.log(doc.data().memberScore);
+        //       console.log(doc.data().memberScore);
 
-                //       this.channel.send("You have " + doc.data().memberScore)
-                //   }
-      
-      switch (reaction.emoji.name) {
-            case '📋': 
-                let profileEmbed = new Discord.RichEmbed()
-                    .setTitle(`**Help commands for Hackweek**`)
-                    .setThumbnail(sIcon)
-                    .addField(`Profile`, `Your current score is: ` + doc.data().memberScore)
-                    .setTimestamp(new Date())
-                    .setFooter(`Requested by ${message.author.tag}`, `${message.author.avatarURL}`)
-            
-                m.edit(profileEmbed);
-            break;
-            
+        //       this.channel.send("You have " + doc.data().memberScore)
+        //   }
+
+        switch (reaction.emoji.name) {
+            case '📋':
+                let playerData = await bot.db.collection('players').doc(message.author.id.toString());
+                await playerData.get().then(doc => {
+                        if (!doc.exists) {
+                            console.log('No such document!');
+                        } else {
+                            let profileEmbed = new Discord.RichEmbed()
+                                .setTitle(`**Help commands for Hackweek**`)
+                                .setThumbnail(sIcon)
+                                .addField(`Profile`, `Your current score is: ` + doc.data().memberScore)
+                                .setTimestamp(new Date())
+                                .setFooter(`Requested by ${message.author.tag}`, `${message.author.avatarURL}`)
+                            m.edit(profileEmbed);
+                            console.log('Document data:', doc.data());
+                            console.log(doc.data().memberScore);
+
+                        }
+                    })
+                    .catch(err => {
+                        console.log('Error getting document', err);
+                    });
+
+                break;
+
             case '🎮':
-                let gamesEmbed = new Discord.RichEmbed()
+                let gamesEmbed = await new Discord.RichEmbed()
                     .setTitle(`**Help commands for Hackweek**`)
                     .setThumbnail(sIcon)
                     .addField(`**GAMES**`)
                     .setTimestamp(new Date())
                     .setFooter(`Requested by ${message.author.tag}`, `${message.author.avatarURL}`)
-        
+
                 m.edit(gamesEmbed);
-            break;
+                break;
 
             case '🎵':
                 let musicEmbed = new Discord.RichEmbed()
@@ -79,9 +92,9 @@ module.exports.run = async (bot, message, args) => {
                     .addField(`**MUSIC**`)
                     .setTimestamp(new Date())
                     .setFooter(`Requested by ${message.author.tag}`, `${message.author.avatarURL}`)
-        
+
                 m.edit(musicEmbed);
-            break;
+                break;
 
             case '🛠':
                 let moderationEmbed = new Discord.RichEmbed()
@@ -92,15 +105,14 @@ module.exports.run = async (bot, message, args) => {
                     .setFooter(`Requested by ${message.author.tag}`, `${message.author.avatarURL}`)
 
                 m.edit(moderationEmbed);
-            break;
+                break;
 
         }
-        
+
     })
-// })
+    // })
 }
-    
-    module.exports.help = {
-      name: "help"
-    }
-    
+
+module.exports.help = {
+    name: "help"
+}
