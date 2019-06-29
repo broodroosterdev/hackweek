a7f917896556f6d4b2e068652cb4b884c0391acc
ScientistMonkey <35115220+ScientistMonkey@users.noreply.github.com>
2019-06-29 02:17:27 +0200

    updated serverinfo.js and readme.md

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -3,14 +3,16 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let sicon = message.guild.iconURL;
     message.delete(250)
+    let mmj = message.member.joinedAt;
+    let mgj = message.guild.createdAt;
     let serverembed = new Discord.RichEmbed()
         .setTitle(`**Server information**`)
         //.setDescription("Server information")
-        .setColor("#50ACF9")
+        .setColor("#FFFFFF")
         .setThumbnail(sicon)
         .addField(`Server Name`, message.guild.name)
-        .addField(`Created on `, message.guild.createdAt)
-        .addField(`You joined this server at `, message.member.joinedAt)
+        .addField(`Created on `, `${mgj.getDate()}/${mgj.getMonth()+1}/${mgj.getFullYear()} at ${mgj.getHours()}:${mgj.getMinutes()}:${mgj.getSeconds()}`)
+        .addField(`You have joined this server on`, `${mmj.getDate()}/${mmj.getMonth()+1}/${mmj.getFullYear()} at ${mmj.getHours()}:${mmj.getMinutes()}:${mmj.getSeconds()}`)
         .addField(`Total members: `, message.guild.memberCount)
         .setTimestamp(new Date())
         .setFooter(`Requested by ${message.author.tag}`, `${message.author.avatarURL}`)
